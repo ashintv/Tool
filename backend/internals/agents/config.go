4b3a2c1b60dc3b890712b49cb84e26596225e752
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2025-11-30 23:55:08 +0530

    feat: added watchdog structure

--- backend/internals/agents/config.go
+++ backend/internals/agents/config.go
@@ -1,6 +1,5 @@
 package agents
 
-
 type CommanderConfig struct {
 	//u := url.URL{Scheme: "ws", Host: "localhost:8080", Path: "/agent-ws"}
 	WsServerHOST         string // WebSocket server URL
---
+++ backend/internals/services/watch.go
@@ -0,0 +1,50 @@
+package services
+
+import "github.com/gorilla/websocket"
+
+//a routine tha frequently checks the health of various services and reports them if they are unresponsive.
+
+type WatchdogService struct {
+	Containers map[string]string  // containers to watch
+	Interval    int                // check interval in seconds
+	Notify      func() 
+	conn 		*websocket.Conn            // alert configurations 	
+}
+
+
+func NewWatchdogService(interval int, notify func()) *WatchdogService {
+	return &WatchdogService{
+		Containers: make(map[string]string),
+		Interval:    interval,
+		Notify:      notify,
+	}
+}
+
+func NewWatchdogServiceWithConnection(interval int, notify func() , conn *websocket.Conn) *WatchdogService {
+	return &WatchdogService{
+		Containers: make(map[string]string),
+		Interval:    interval,
+		Notify:      notify,
+		conn:        conn,
+	}
+}
+
+func (w *WatchdogService) MonitorServices() error {
+
+	return nil
+}
+
+
+func (w *WatchdogService) ContainerWatch(containerID string){
+	// fetcth container runnnig
+	// cross check with stored status
+	// add new to map if not present notify
+	// anything missess report notify
+}
+
+
+func (w *WatchdogService) ResourceWatch(notify func()){
+	// monitor resource usage
+	// if threshold crossed notify
+	// STREAM RESOURCE USAGE LOGS
+}
