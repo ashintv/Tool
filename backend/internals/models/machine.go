12720e50d5c4f8510aaa551604ec2f1ad4d5dee5
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2025-12-01 18:47:46 +0530

    schema: update

--- backend/internals/models/machine.go
+++ backend/internals/models/machine.go
@@ -4,7 +4,7 @@ import "time"
 
 type Machine struct {
 	ID        uint      `gorm:"primaryKey"`
-	MachineID string    `gorm:"unique;not null"`
+	MachineName string    `gorm:"unique;not null"`
 
 	CreatorID uint
 	Creator   User `gorm:"foreignKey:CreatorID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
