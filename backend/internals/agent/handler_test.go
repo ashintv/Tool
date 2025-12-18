0d7db2d538944a729bb1425c17f3ab6d9b0f1ff1
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2025-12-18 11:17:11 +0530

    tests: StartContainer

--- backend/internals/agent/handler_test.go
+++ backend/internals/agent/handler_test.go
@@ -634,4 +634,83 @@ func TestHandleRestartContainer(t *testing.T) {
 	}
 }
 
-func TestHandleStartContainer(t *testing.T) {}
+func TestHandleStartContainer(t *testing.T) {
+	tests := []Test{
+		{
+			name: "Successful starting of container",
+			mockClient: &MockDockerClient{
+				ContainerStartFn: func(
+					ctx context.Context,
+					containerID string,
+					options container.StartOptions,
+				) error {
+					if containerID != "test_container" {
+						return fmt.Errorf(`expected container Id to be: "test_container" , got %s `, containerID)
+					}
+					return nil
+				},
+			},
+			req: lib.Command{
+				CMD:         lib.START_CONTAINER,
+				MachineID:   "test-machine",
+				ContainerID: "test_container",
+			},
+			expectError: false,
+		},
+		{
+			name: "Error starting container",
+			mockClient: &MockDockerClient{
+				ContainerStartFn: func(
+					ctx context.Context,
+					containerID string,
+					options container.StartOptions,
+				) error {
+					return fmt.Errorf("Failed to start")
+				},
+			},
+			req: lib.Command{
+				CMD:         lib.START_CONTAINER,
+				MachineID:   "test-machine",
+				ContainerID: "test_container",
+			},
+			expectError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+
+			handler := NewHandler(tt.mockClient)
+			resp := handler.HandleStartContainer(ctx, tt.req)
+
+
+			if tt.expectError {
+				if resp.Payload.Error == "" {
+					t.Fatal("Error expected got no error")
+				}
+				return
+			}
+
+			if !tt.expectError {
+				if resp.Payload.Error != "" {
+					t.Fatalf("Error unexpected got error: %s", resp.Payload.Error)
+				}
+				return
+			}
+
+			data, ok := resp.Payload.Data.(string)
+			if !ok {
+				t.Fatalf("payload data is not string")
+				return
+			}
+			expectedMessage := "Container started successfully"
+			if data != expectedMessage {
+				t.Fatalf("expected payload data: %s, got: %s", expectedMessage, data)
+			}
+
+		})
+	}
+}
